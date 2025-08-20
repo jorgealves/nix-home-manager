2969fc0197a4fe20b024dd227fbad4a882745f1e
Jorge Alves <jorgeandrealves@gmail.com>
2025-08-20 17:28:00 +0100

    ops(nvim): reset config

---
+++ dotfiles/nvim/lua/plugins/codecompanion.nvim.lua
@@ -0,0 +1,31 @@
+return {
+  "olimorris/codecompanion.nvim",
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "copilot",
+      },
+      inline = {
+        adapter = "copilot",
+      },
+      cmd = {
+        adapter = "copilot",
+      },
+    },
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_result_in_chat = true,
+        },
+      },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "ravitemer/mcphub.nvim",
+  },
+}
---
+++ dotfiles/nvim/lua/plugins/hardtime.nvim.lua
@@ -0,0 +1,8 @@
+return {
+  "m4xshen/hardtime.nvim",
+  opts = {
+    max_count = 3,
+    hint = true,
+    disable_mouse = true,
+  },
+}
